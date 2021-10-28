0e1e459562b2e10d7b64d1407dc0c8d4fdb74a22
Prabhjot <pjsingh1359@gmail.com>
2021-10-27 22:44:32 -0700

    laid out form to search flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,16 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all
-    @airport_options = Airport.all.map { |a| [a.full_name, a.id] }
+    @airport_options = Airport.all.map { |a| [a.name, a.id] }
 
     # return if flight_params.empty?
 
     @flights = Flight.where(flight_params)
+  end
+
+  private
+
+  def flight_params
+    params.permit(:origin_id, :destination_id, :departure_date, :departure_time)
   end
 end
